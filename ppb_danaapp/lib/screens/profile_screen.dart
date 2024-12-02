4fba921c494585dcc6188809c709ada72502acb0
HamidKhaeruman <hamidkhaeruman28@gmail.com>
2024-12-02 16:31:55 +0700

    penyesuaian profile_screen.dart

--- ppb_danaapp/lib/screens/profile_screen.dart
+++ ppb_danaapp/lib/screens/profile_screen.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Profile Page',
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(),
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: ProfilePage(),
-    );
-  }
-}
-
-class ProfilePage extends StatelessWidget {
+class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
